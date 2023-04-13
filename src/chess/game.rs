620134aaf72e39d3b7a74e8fd51e3c2ae3cc355d
Adam Allsebrook <adsorigin@gmail.com>
2023-04-13 19:16:35 +0100

    Refactor piece moves into the pieces.rs

--- src/chess/game.rs
+++ src/chess/game.rs
@@ -1,4 +1,4 @@
-use crate::chess::{Board, Piece, PieceType, Square};
+use crate::chess::{Board, Piece, Square};
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -26,83 +26,13 @@ impl Game {
         }
     }
 
-    // Get all possible moves for a piece, ignoring the state of the board after the move
-    fn get_possible_moves(&self, square: &Square, piece: &Piece) -> Vec<Move> {
-        let mut moves = Vec::<Move>::new();
-        match piece.piece_type {
-            PieceType::Pawn => {
-                // Get the direction the pawn can move in
-                let forward = match piece.color {
-                    Color::White => 1,
-                    Color::Black => -1,
-                };
-
-                // Check if we can push the pawn forwards
-                let valid_square = self.board.validate_square_offset(&square, 0, forward);
-                if let Some(new_square) = valid_square {
-                    if self.board.get(new_square).is_none() {
-                        moves.push(Move {
-                            from: square.clone(),
-                            to: new_square,
-                        });
-                    }
-                }
-
-                // Check if we can take a piece diagonally
-                for file_offset in (-1..=1).step_by(2) {
-                    let valid_square =
-                        self.board
-                            .validate_square_offset(&square, file_offset, forward);
-                    if let Some(new_square) = valid_square {
-                        if let Some(taken_piece) = self.board.get(new_square) {
-                            if taken_piece.color != piece.color {
-                                moves.push(Move {
-                                    from: square.clone(),
-                                    to: new_square,
-                                });
-                            }
-                        }
-                    }
-                }
-            }
-            PieceType::Knight => {
-                for file_offset in -2i8..=2 {
-                    for rank_offset in -2i8..=2 {
-                        if file_offset == 0
-                            || rank_offset == 0
-                            || file_offset.abs() == rank_offset.abs()
-                        {
-                            continue;
-                        }
-                        if let Some(new_square) =
-                            self.board
-                                .validate_square_offset(&square, file_offset, rank_offset)
-                        {
-                            if let Some(taken_piece) = self.board.get(new_square) {
-                                if taken_piece.color == piece.color {
-                                    continue;
-                                }
-                            }
-                            moves.push(Move {
-                                from: square.clone(),
-                                to: new_square,
-                            });
-                        }
-                    }
-                }
-            }
-            _ => (),
-        }
-        moves
-    }
-
     // Get all possible moves for every piece for the current player,
     // ignoring the state of the board after the move
     pub fn get_all_possible_moves(&self) -> Vec<Move> {
         let mut moves = Vec::<Move>::new();
         for (square, piece) in self.board.get_all_pieces() {
             if piece.color == self.turn {
-                moves.append(&mut self.get_possible_moves(&square, &piece));
+                moves.append(&mut piece.get_moves(&self.board, &square));
             }
         }
         moves
--- src/chess/pieces.rs
+++ src/chess/pieces.rs
@@ -1,4 +1,4 @@
-use crate::chess::Color;
+use crate::chess::{Board, Color, Move, Square};
 
 #[derive(Debug, Copy, Clone)]
 pub enum PieceType {
@@ -14,6 +14,94 @@ pub enum PieceType {
 pub struct Piece {
     pub piece_type: PieceType,
     pub color: Color,
+}
+
+impl Piece {
+    pub fn get_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        match self.piece_type {
+            PieceType::Pawn => self.get_pawn_moves(board, from_square),
+            PieceType::Knight => self.get_knight_moves(board, from_square),
+            PieceType::Bishop => self.get_bishop_moves(board, from_square),
+            PieceType::Rook => self.get_rook_moves(board, from_square),
+            PieceType::Queen => self.get_queen_moves(board, from_square),
+            PieceType::King => self.get_king_moves(board, from_square),
+        }
+    }
+
+    fn get_pawn_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        let mut moves = Vec::new();
+        // Get the direction the pawn can move in
+        let forward = match self.color {
+            Color::White => 1,
+            Color::Black => -1,
+        };
+
+        // Check if we can push the pawn forwards
+        let valid_square = board.validate_square_offset(from_square, 0, forward);
+        if let Some(new_square) = valid_square {
+            if board.get(new_square).is_none() {
+                moves.push(Move {
+                    from: from_square.clone(),
+                    to: new_square,
+                });
+            }
+        }
+
+        // Check if we can take a piece diagonally
+        for file_offset in (-1..=1).step_by(2) {
+            let valid_square = board.validate_square_offset(from_square, file_offset, forward);
+            if let Some(new_square) = valid_square {
+                if let Some(taken_piece) = board.get(new_square) {
+                    if taken_piece.color != self.color {
+                        moves.push(Move {
+                            from: new_square.clone(),
+                            to: new_square,
+                        });
+                    }
+                }
+            }
+        }
+        moves
+    }
+    fn get_knight_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        let mut moves = Vec::new();
+        // The knight moves by checking every square in a 5x5 box centered on itself,
+        // only moving to squares that are 2 squares away in one direction and 1 square away in the other
+        for file_offset in -2i8..=2 {
+            for rank_offset in -2i8..=2 {
+                if file_offset == 0 || rank_offset == 0 || file_offset.abs() == rank_offset.abs() {
+                    continue;
+                }
+                if let Some(new_square) =
+                    board.validate_square_offset(from_square, file_offset, rank_offset)
+                {
+                    // We cannot move to a square occupied by one of our own pieces
+                    if let Some(taken_piece) = board.get(new_square) {
+                        if taken_piece.color == self.color {
+                            continue;
+                        }
+                    }
+                    moves.push(Move {
+                        from: from_square.clone(),
+                        to: new_square,
+                    });
+                }
+            }
+        }
+        moves
+    }
+    fn get_bishop_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        Vec::new()
+    }
+    fn get_rook_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        Vec::new()
+    }
+    fn get_queen_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        Vec::new()
+    }
+    fn get_king_moves(&self, board: &Board, from_square: &Square) -> Vec<Move> {
+        Vec::new()
+    }
 }
 
 #[macro_export]
